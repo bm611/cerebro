1dab91f0522d7e36b90e53984dd388aeeab4f006
Bharath Mohan <broxdeez@Mac.attlocal.net>
2024-10-17 21:45:34 -0700

    more UI changes

--- cerebro/cerebro.py
+++ cerebro/cerebro.py
@@ -64,6 +64,7 @@ class State(rx.State):
 
     # for handling radio button option
     def handle_selection(self, value: str):
+        self.show_answer = False
         self.selected_option = value
 
     def set_query(self, query: str):
@@ -111,16 +112,16 @@ def index() -> rx.Component:
         rx.vstack(
             rx.heading(
                 "Cerebro",
-                class_name="text-6xl font-normal",
+                class_name="text-6xl font-normal text-black",
             ),
             rx.spacer(),
             rx.heading(
                 "Generate quiz based on prompt or by uploading a document",
-                class_name="mt-10 text-3xl font-normal text-center",
+                class_name="mt-10 text-3xl font-normal text-center text-black",
             ),
             rx.text(
-                "Generate quizzes effortlessly with our AI-powered tool. Simply upload a document or provide a prompt, and we'll create a tailored quiz to reinforce learning and assess understanding.",
-                class_name="mt-4 text-lg text-gray-500 text-center max-w-2xl mx-auto",
+                "Generate quizzes effortlessly with AI-powered tool. Simply upload a document or provide a prompt, and get a tailored quiz to reinforce learning and assess understanding.",
+                class_name="md:px-0 px-2 mt-4 text-lg text-gray-500 text-center max-w-2xl mx-auto",
             ),
             rx.vstack(
                 rx.segmented_control.root(
@@ -136,14 +137,14 @@ def index() -> rx.Component:
                             "Drag and drop files here or click to select files",
                             class_name="text-center",
                         ),
-                        class_name="w-3/4 h-40 p-2 border border-blue-600 rounded-2xl mt-4",
+                        class_name="w-full md:w-3/4 h-40 p-2 border border-blue-600 rounded-2xl mt-4",
                     ),
                     rx.vstack(
                         rx.text_area(
                             placeholder="Enter your prompt here...",
                             value=State.query,
                             on_change=State.set_query,
-                            class_name="bg-transparent w-3/4 h-40 p-2 border border-blue-600 rounded-2xl mt-4",
+                            class_name="bg-transparent w-full md:w-3/4 h-40 p-2 border border-blue-600 rounded-2xl mt-4 text-black",
                         ),
                         rx.button(
                             "Generate Quiz",
@@ -176,7 +177,7 @@ def render_quiz() -> rx.Component:
         rx.vstack(
             rx.heading(
                 "Cerebro",
-                class_name="mb-20 text-6xl font-normal hover:cursor-pointer",
+                class_name="mb-20 text-6xl text-black font-normal hover:cursor-pointer",
                 on_click=rx.redirect("/"),
             ),
             rx.hstack(
@@ -211,12 +212,8 @@ def render_quiz() -> rx.Component:
                     ),
                 ),
                 rx.text(State.current_question["quiz_title"]),
-                rx.text(
-                    State.display_index,
-                    rx.text.strong("/10"),
-                    as_="span",
-                ),
-                class_name="w-3/4 justify-between items-center px-4",
+                rx.text(f"{State.display_index}/{State.response.length().to_string()}"),
+                class_name="md:w-3/4 justify-between items-center px-2 w-full md:md:px-4",
             ),
             rx.box(
                 rx.vstack(
@@ -235,33 +232,52 @@ def render_quiz() -> rx.Component:
                     ),
                     class_name="p-4",
                 ),
-                class_name="border-2 border-black w-3/4 rounded-3xl",
+                class_name="border-2 border-black w-full md:w-3/4 rounded-3xl",
+            ),
+            rx.cond(
+                State.show_answer,
+                rx.cond(
+                    State.selected_option == State.current_question["answer"],
+                    rx.badge(
+                        rx.icon("circle-check", size=18),
+                        "Correct",
+                        color_scheme="grass",
+                        variant="solid",
+                        high_contrast=False,
+                        radius="full",
+                        size="3",
+                    ),
+                    rx.badge(
+                        rx.icon("circle-x", size=18),
+                        "Wrong",
+                        color_scheme="tomato",
+                        variant="solid",
+                        high_contrast=False,
+                        radius="full",
+                        size="3",
+                    ),
+                ),
+                rx.text(""),
             ),
             rx.button(
                 "Check Answer",
                 on_click=State.check_answer,
                 class_name="mt-4 px-4 py-2 bg-gray-600 hover:bg-gray-800 text-white rounded-md",
             ),
-            rx.cond(
-                State.show_answer,
-                rx.cond(
-                    State.selected_option == State.current_question["answer"],
-                    rx.text("correct"),
-                    rx.text("incorrect"),
-                ),
-                rx.text(""),
-            ),
             rx.hstack(
                 rx.button(
                     rx.icon("chevron-left", color="black"),
-                    variant="ghost",
+                    variant="outline",
                     on_click=State.previous_question,
                 ),
+                rx.spacer(),
+                rx.spacer(),
                 rx.button(
                     rx.icon("chevron-right", color="black"),
-                    variant="ghost",
+                    variant="outline",
                     on_click=State.next_question,
                 ),
+                class_name="mt-6",
             ),
             class_name="w-full mt-10 flex items-center justify-center",
         ),
@@ -279,4 +295,8 @@ app = rx.App(
     stylesheets=[
         "/fonts/font.css",  # This path is relative to assets/
     ],
+    theme=rx.theme(
+        appearance="light",
+        has_background=True,
+    ),
 )
