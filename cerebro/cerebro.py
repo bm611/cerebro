735cd169f5b53f3dfb4adbed165dcb619ac917f0
Bharath Mohan <broxdeez@Bharaths-MacBook-Pro.local>
2024-10-19 13:47:15 -0700

    example grid and score card after quiz completion

--- cerebro/cerebro.py
+++ cerebro/cerebro.py
@@ -67,6 +67,8 @@ class State(rx.State):
     selected_option: str = ""
     # is_correct: bool = False
     show_answer: bool = False
+    num_correct: int = 0
+    is_quiz_end: bool = False
 
     difficulty: str = ""
     num_questions: str = ""
@@ -94,6 +96,8 @@ class State(rx.State):
         self.difficulty = ""
         self.num_questions = ""
         self.show_answer = False
+        self.num_correct = 0
+        self.is_quiz_end = False
         return rx.redirect("/")
 
     def handle_submit(self):
@@ -114,6 +118,10 @@ class State(rx.State):
 
     def check_answer(self):
         self.show_answer = True
+        if self.selected_option == self.current_question["answer"]:
+            self.num_correct += 1
+        if self.display_index == len(self.response):
+            self.is_quiz_end = True
 
     @rx.var
     def display_index(self) -> int:
@@ -128,22 +136,22 @@ class State(rx.State):
         return self.current_question["choices"]
 
 
-@rx.page(route="/", title="home")
+@rx.page(route="/", title="Cerebro")
 def index() -> rx.Component:
     return rx.container(
         rx.vstack(
             rx.heading(
                 "Cerebro",
-                class_name="text-6xl font-normal text-black",
+                class_name="text-4xl md:text-6xl font-normal text-black",
             ),
             rx.spacer(),
             rx.heading(
                 "Generate quiz based on prompt or by uploading a document",
-                class_name="mt-10 text-3xl font-normal text-center text-black",
+                class_name="mt-4 md:mt-10 text-xl md:text-3xl font-normal text-center text-black",
             ),
             rx.text(
                 "Generate quizzes effortlessly with AI-powered tool. Simply upload a document or provide a prompt, and get a tailored quiz to reinforce learning and assess understanding.",
-                class_name="md:px-0 px-2 mt-4 text-lg text-gray-500 text-center max-w-2xl mx-auto",
+                class_name="hidden md:block md:px-0 px-2 mt-4 text-lg text-gray-500 text-center max-w-2xl mx-auto",
             ),
             rx.vstack(
                 rx.segmented_control.root(
@@ -161,12 +169,51 @@ def index() -> rx.Component:
                         class_name="w-1/2 mr-2",
                     ),
                     rx.select(
-                        ["5", "10", "15", "20"],
+                        ["1", "5", "10", "15", "20"],
                         placeholder="Select Num questions",
                         on_change=State.set_num_questions,
                         value=State.num_questions,
                         class_name="w-1/2 ml-2",
                     ),
+                ),
+                rx.grid(
+                    rx.card(
+                        rx.hstack(
+                            rx.icon("landmark", size=24),
+                            rx.text(
+                                "Ancient Roman History",
+                            ),
+                        ),
+                        class_name="p-4 hover:shadow-lg transition-shadow cursor-pointer",
+                    ),
+                    rx.card(
+                        rx.hstack(
+                            rx.icon("atom", size=24),
+                            rx.text(
+                                "Intro to Quantum Physics",
+                            ),
+                        ),
+                        class_name="p-4 hover:shadow-lg transition-shadow cursor-pointer",
+                    ),
+                    rx.card(
+                        rx.hstack(
+                            rx.icon("globe", size=24),
+                            rx.text(
+                                "World Geography",
+                            ),
+                        ),
+                        class_name="p-4 hover:shadow-lg transition-shadow cursor-pointer",
+                    ),
+                    rx.card(
+                        rx.hstack(
+                            rx.icon("book", size=24),
+                            rx.text(
+                                "Classic Literature",
+                            ),
+                        ),
+                        class_name="p-4 hover:shadow-lg transition-shadow cursor-pointer",
+                    ),
+                    class_name="mt-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-4",
                 ),
                 rx.cond(
                     SegmentedState.control == "file",
@@ -212,112 +259,161 @@ def render_todo() -> rx.Component:
 @rx.page(route="quiz", title="quiz")
 def render_quiz() -> rx.Component:
     return rx.container(
-        rx.vstack(
-            rx.heading(
-                "Cerebro",
-                class_name="mb-20 text-6xl text-black font-normal hover:cursor-pointer",
-                on_click=State.reset_vars,
-            ),
-            rx.hstack(
-                rx.match(
-                    State.current_question["difficulty"],
-                    (
-                        "easy",
-                        rx.badge(
-                            "easy",
-                            color_scheme="grass",
-                            variant="outline",
-                            high_contrast=False,
-                        ),
-                    ),
-                    (
-                        "medium",
-                        rx.badge(
-                            "medium",
-                            color_scheme="yellow",
-                            variant="outline",
-                            high_contrast=False,
-                        ),
-                    ),
-                    (
-                        "hard",
-                        rx.badge(
-                            "hard",
-                            color_scheme="tomato",
-                            variant="outline",
-                            high_contrast=False,
-                        ),
-                    ),
-                ),
-                rx.text(State.current_question["quiz_title"]),
-                rx.text(f"{State.display_index}/{State.response.length().to_string()}"),
-                class_name="md:w-3/4 justify-between items-center px-2 w-full md:md:px-4",
-            ),
-            rx.box(
+        rx.cond(
+            State.is_quiz_end,
+            rx.center(
                 rx.vstack(
-                    rx.text("Question", class_name="text-sm"),
-                    rx.card(
-                        rx.text(State.current_question["question"]),
-                        class_name="w-full text-sm bg-gray-400",
+                    rx.heading(
+                        "Cerebro",
+                        class_name="mb-20 text-4xl md:text-6xl text-black font-normal hover:cursor-pointer",
+                        on_click=State.reset_vars,
                     ),
-                    rx.radio(
-                        State.current_choices,
-                        value=State.selected_option,
-                        on_change=State.handle_selection,
-                        direction="column",
-                        spacing="2",
-                        size="2",
+                    rx.image(
+                        src="/cat.webp",
+                        class_name="bg-transparent",
+                        width="200px",
+                        height="auto",
                     ),
-                    class_name="p-4",
+                    rx.hstack(
+                        rx.text(
+                            "You scored",
+                            class_name="font-semibold text-2xl",
+                        ),
+                        rx.text(
+                            f"{State.num_correct}/{State.response.length().to_string()}",
+                            class_name="text-2xl font-semibold",
+                        ),
+                    ),
+                    rx.button(
+                        "Another Quiz?",
+                        on_click=State.reset_vars,
+                        class_name="mt-4 px-4 py-2 bg-gray-600 hover:bg-gray-800 text-white rounded-md",
+                    ),
+                    class_name="w-full mt-10 flex items-center justify-center",
                 ),
-                class_name="border-2 border-black w-full md:w-3/4 rounded-3xl",
             ),
-            rx.cond(
-                State.show_answer,
+            rx.vstack(
+                rx.heading(
+                    "Cerebro",
+                    class_name="mb-20 text-4x md:text-6xl text-black font-normal hover:cursor-pointer",
+                    on_click=State.reset_vars,
+                ),
+                rx.hstack(
+                    rx.match(
+                        State.current_question["difficulty"],
+                        (
+                            "easy",
+                            rx.badge(
+                                "easy",
+                                color_scheme="grass",
+                                variant="outline",
+                                high_contrast=False,
+                            ),
+                        ),
+                        (
+                            "medium",
+                            rx.badge(
+                                "medium",
+                                color_scheme="yellow",
+                                variant="outline",
+                                high_contrast=False,
+                            ),
+                        ),
+                        (
+                            "hard",
+                            rx.badge(
+                                "hard",
+                                color_scheme="tomato",
+                                variant="outline",
+                                high_contrast=False,
+                            ),
+                        ),
+                    ),
+                    rx.text(State.current_question["quiz_title"]),
+                    rx.text(
+                        f"{State.display_index}/{State.response.length().to_string()}"
+                    ),
+                    class_name="md:w-3/4 justify-between items-center px-2 w-full md:md:px-4",
+                ),
+                rx.box(
+                    rx.vstack(
+                        rx.text("Question", class_name="text-sm"),
+                        rx.card(
+                            rx.text(State.current_question["question"]),
+                            class_name="w-full text-sm bg-gray-400",
+                        ),
+                        rx.radio(
+                            State.current_choices,
+                            value=State.selected_option,
+                            on_change=State.handle_selection,
+                            direction="column",
+                            spacing="2",
+                            size="2",
+                        ),
+                        class_name="p-4",
+                    ),
+                    class_name="border-2 border-black w-full md:w-3/4 rounded-3xl",
+                ),
                 rx.cond(
-                    State.selected_option == State.current_question["answer"],
-                    rx.badge(
-                        rx.icon("circle-check", size=18),
-                        "Correct",
-                        color_scheme="grass",
-                        variant="solid",
-                        high_contrast=False,
-                        radius="full",
-                        size="3",
+                    State.show_answer,
+                    rx.cond(
+                        State.selected_option == State.current_question["answer"],
+                        rx.badge(
+                            rx.icon("circle-check", size=18),
+                            "Correct",
+                            color_scheme="grass",
+                            variant="solid",
+                            high_contrast=False,
+                            radius="full",
+                            size="3",
+                        ),
+                        rx.badge(
+                            rx.icon("circle-x", size=18),
+                            "Wrong",
+                            color_scheme="tomato",
+                            variant="solid",
+                            high_contrast=False,
+                            radius="full",
+                            size="3",
+                        ),
                     ),
-                    rx.badge(
-                        rx.icon("circle-x", size=18),
-                        "Wrong",
-                        color_scheme="tomato",
-                        variant="solid",
-                        high_contrast=False,
-                        radius="full",
-                        size="3",
+                    rx.text(""),
+                ),
+                rx.button(
+                    "Check Answer",
+                    on_click=State.check_answer,
+                    class_name="mt-4 px-4 py-2 bg-gray-600 hover:bg-gray-800 text-white rounded-md",
+                ),
+                rx.cond(
+                    State.is_quiz_end,
+                    rx.vstack(
+                        rx.text(
+                            "You scored",
+                            rx.text(
+                                f"{State.num_correct}/{State.response.length().to_string()}"
+                            ),
+                            as_="span",
+                        ),
+                        rx.button("Another Quiz?", href="/"),
+                    ),
+                    rx.hstack(
+                        rx.button(
+                            rx.icon("chevron-left", color="black"),
+                            variant="outline",
+                            on_click=State.previous_question,
+                        ),
+                        rx.spacer(),
+                        rx.spacer(),
+                        rx.button(
+                            rx.icon("chevron-right", color="black"),
+                            variant="outline",
+                            on_click=State.next_question,
+                        ),
+                        class_name="mt-6",
                     ),
                 ),
-                rx.text(""),
+                class_name="w-full mt-10 flex items-center justify-center",
             ),
-            rx.button(
-                "Check Answer",
-                on_click=State.check_answer,
-                class_name="mt-4 px-4 py-2 bg-gray-600 hover:bg-gray-800 text-white rounded-md",
-            ),
-            rx.hstack(
-                rx.button(
-                    rx.icon("chevron-left", color="black"),
-                    variant="outline",
-                    on_click=State.previous_question,
-                ),
-                rx.spacer(),
-                rx.spacer(),
-                rx.button(
-                    rx.icon("chevron-right", color="black"),
-                    variant="outline",
-                    on_click=State.next_question,
-                ),
-                class_name="mt-6",
-            ),
-            class_name="w-full mt-10 flex items-center justify-center",
         ),
     )
 
